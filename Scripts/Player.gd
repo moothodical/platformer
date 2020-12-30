a4c5bded591469c9c141fc5abc7590238a6be9b1
baderc0 <andreibaderca1@gmail.com>
2020-12-30 15:41:23 -0500

    got wall sliding kinda working
    used game endeavors tutorial on YT

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,14 +1,20 @@
 extends KinematicBody2D
 
+onready var left_wall_raycasts = $WallRaycasts/LeftWallRaycasts
+onready var right_wall_raycasts = $WallRaycasts/RightWallRaycasts
+
 const UP = Vector2(0, -1)
-const MAX_JUMP_VEL = -350
+const MAX_JUMP_VEL = -250
 const MIN_JUMP_VEL = -100
-const MAX_SPEED = 100
+const MAX_SPEED = 300
+const WALL_JUMP_VEL = Vector2(MAX_SPEED, -300)
 
 var velocity = Vector2()
 var move_speed = 100
-var gravity = 1000
+var gravity = 850
 var is_grounded
+var wall_direction = 1
+var move_dir
 
 func apply_movement():
 	velocity = move_and_slide(velocity, UP) 
@@ -18,20 +24,48 @@ func apply_movement():
 func apply_gravity(delta):
 	velocity.y += gravity * delta
 
+func wall_jump():
+	var wall_jump_velocity = WALL_JUMP_VEL
+	wall_jump_velocity.x *= -wall_direction
+	velocity = wall_jump_velocity
+
 func _get_h_weight():
 	return 0.2 if is_grounded else 0.1 # player has less control in the air
 
+func _update_move_direction():
+	move_dir = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
+
 func _handle_move_input():
-	var move_dir = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
 	velocity.x = lerp(velocity.x, MAX_SPEED * move_dir, _get_h_weight())
 	
 	# Flip horizontally if facing left, otherwise default face right
 	if move_dir == -1: # if facing left
-		print("entered if")
 		get_node("CharacterRig/Torso").set_flip_h(true)
 	elif move_dir == 1: # facing right
 		get_node("CharacterRig/Torso").set_flip_h(false)
 
 func check_is_grounded():
 	return true if $GroundedRaycast.is_colliding() else false
+
+func _update_wall_direction():
+	var is_near_wall_left = check_is_valid_wall(left_wall_raycasts)
+	var is_near_wall_right = check_is_valid_wall(right_wall_raycasts)
+	
+	if is_near_wall_left && is_near_wall_right:
+		wall_direction = move_dir
+	else:
+		wall_direction = -int(is_near_wall_left) + int(is_near_wall_right)
+	
+func check_is_valid_wall(wall_raycasts):
+	for raycast in wall_raycasts.get_children():
+		if raycast.is_colliding():
+			#var dot = acos(Vector2.UP.dot(raycast.get_collision_normal()))
+			#if dot > PI * 0.35 && dot < PI * 0.55:
+			return true
+	return false
+	
+
+
+
+
 
--- Scripts/PlayerFSM.gd
+++ Scripts/PlayerFSM.gd
@@ -6,24 +6,33 @@ func _ready():
 	add_state("run")
 	add_state("jump")
 	add_state("fall")
+	add_state("wall_slide")
 	call_deferred("set_state", states.idle)
 
 func _input(event):
 	if event.is_action_pressed("jump") && parent.is_grounded:
 		parent.velocity.y = parent.MAX_JUMP_VEL
-	if state == states.jump:
+	elif state == states.wall_slide:
+		if event.is_action_pressed("jump"):
+			parent.wall_jump()
+			set_state(states.jump)
+	elif state == states.jump:
 		if event.is_action_released("jump") && parent.velocity.y < parent.MIN_JUMP_VEL: ## if jumping
 			parent.velocity.y = parent.MIN_JUMP_VEL
+	
 
 func _state_logic(delta):
-	parent._handle_move_input()
+	parent._update_move_direction()
+	parent._update_wall_direction()
+	if state != states.wall_slide:
+		parent._handle_move_input()
 	parent.apply_gravity(delta)
 	parent.apply_movement()
 
 func _get_transition(delta):
 	match state:
 		states.idle:
-			if !parent.is_grounded:
+			if !parent.check_is_grounded():
 				if parent.velocity.y < 0:
 					return states.jump
 				elif parent.velocity.y > 0:
@@ -31,7 +40,7 @@ func _get_transition(delta):
 			elif abs(parent.velocity.x) > parent.MAX_SPEED / 4:
 				return states.run
 		states.run:
-			if !parent.is_grounded:
+			if !parent.check_is_grounded():
 				if parent.velocity.y < 0:
 					return states.jump
 				elif parent.velocity.y > 0:
@@ -39,15 +48,25 @@ func _get_transition(delta):
 			elif abs(parent.velocity.x) < parent.MAX_SPEED / 4:
 				return states.idle
 		states.jump:
-			if parent.is_grounded:
+			print(parent.wall_direction)
+			if parent.wall_direction != 0:
+				return states.wall_slide
+			elif parent.is_grounded:
 				return states.idle
 			elif parent.velocity.y >= 0:
 				return states.fall
 		states.fall:
-			if parent.is_grounded:
+			if parent.wall_direction != 0:
+				return states.wall_slide
+			elif parent.is_grounded:
 				return states.idle
 			elif parent.velocity.y <  0:
 				return states.jump
+		states.wall_slide:
+			if parent.wall_direction == 0:
+				return states.fall
+			
+			
 	
 	return null
 
@@ -62,6 +81,10 @@ func _enter_state(new_state, old_state):
 		anim.play("jump")
 	elif new_state == states.fall:
 		anim.play("fall")
+	elif new_state == states.wall_slide:
+		pass
+		# play wall slide anim
+		#parent.set_flip_h(true)
 	
 func _exit_state(old_state, new_state):
 	if new_state != states.run:
